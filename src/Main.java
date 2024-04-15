3ec1ebe79fca1a4d944c76c8b21b621f5012649e
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-15 22:06:30 +0530

    double b variable.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner  = new Scanner(System.in);
         double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
     }
 }
