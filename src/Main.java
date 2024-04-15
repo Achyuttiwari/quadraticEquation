db249a767cd3f87a41d90c19a203cc7b3db2c198
Achyut Kumar Tiwari <achyuttiwari34@gmail.com>
2024-04-15 22:05:57 +0530

    double a variable.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner  = new Scanner(System.in);
+        double a = scanner.nextDouble();
     }
 }
